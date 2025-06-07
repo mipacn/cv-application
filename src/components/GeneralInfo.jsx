eca2eaa58dca6eb118c06a183e4b85433ae87e6e
mipacn <martinugaldeminana1@gmail.com>
2025-06-07 21:25:10 +0200

    general functionality, completed, project "finished"

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -3,30 +3,43 @@ export default function GeneralInfo({ info, setInfo }) {
 		const { name, value } = e.target
 		setInfo((prev) => ({ ...prev, [name]: value }))
 	}
-	return (
-		<div>
-			<h3>General information</h3>
-			<input
-				type="text"
-				placeholder="name"
-				name="name"
-				value={info.name}
-				onChange={handleChange}
-			/>
-			<input
-				type="text"
-				placeholder="email"
-				name="email"
-				value={info.email}
-				onChange={handleChange}
-			/>
-			<input
-				type="text"
-				placeholder="phone"
-				name="phone"
-				value={info.phone}
-				onChange={handleChange}
-			/>
-		</div>
-	)
+	const [buttonState, setButtonState] = useState(true)
+	function toggleButton() {
+		setButtonState((prev) => !prev)
+	}
+	if (buttonState) {
+		return (
+			<div>
+				<h3>General information</h3>
+				<button onClick={toggleButton}>Edit general information</button>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<h3>General information</h3>
+				<input
+					type="text"
+					placeholder="name"
+					name="name"
+					value={info.name}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					placeholder="email"
+					name="email"
+					value={info.email}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					placeholder="phone"
+					name="phone"
+					value={info.phone}
+					onChange={handleChange}
+				/>
+			</div>
+		)
+	}
 }
